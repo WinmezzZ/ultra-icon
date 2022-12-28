430384fdae5e6e2cebf7490da26fe1228be40039
Winme <winme0308@gmail.com>
2022-12-28 16:09:33 +0800

    publish v0.0.11

--- scripts/build-code.ts
+++ scripts/build-code.ts
@@ -2,15 +2,25 @@ import fs from 'fs';
 import { execSync } from 'child_process';
 import { rollup } from 'rollup';
 import esbuild from 'rollup-plugin-esbuild';
+import pkg from '../package.json';
 
 fs.rmSync('es', { recursive: true, force: true });
 fs.rmSync('lib', { recursive: true, force: true });
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'prop-type': 'PropTypes',
+};
+const external = Object.keys(globals);
+// https://www.npmjs.com/package/@rollup/plugin-babel/v/5.2.1#babelhelpers
+const esExternals = [...external, /@emotion\/react/, /@babel\/runtime/, ...Object.keys(pkg.dependencies)];
 
 async function build() {
   const bundle = await rollup({
     input: 'src/index.ts',
     plugins: [esbuild()],
-    external: ['react', 'react-dom', 'prop-types', /@emotion\/react/],
+    external: esExternals,
   });
 
   bundle
